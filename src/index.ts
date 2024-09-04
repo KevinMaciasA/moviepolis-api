0ddb6cca64306e80a8dae3cad404a5cda7f6a603
Kevin <kevin.macias.a@gmail.com>
2024-09-04 14:37:21 -0700

    Remove some old comments and restart home.html

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,6 @@ import mysqlPlugin from '@fastify/mysql'
 
 const server = fastify({ logger: true })
 
-// Web API
-// API that serves a webpage html
-// TODO:
-// pages:
-// - home
-// - pages
-//    - read pages from a databases
-//    - post / update / delete
-
 server.register(mysqlPlugin, { connectionString: "mysql://root@localhost/mysql" })
 server.register(rootRoute, { prefix: "/v1" })
 server.register(pagesRoute, { prefix: "/v1/pages" })
