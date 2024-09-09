e2886f124e1b21e9d28586bb1390d15aca1c3f61
Kevin <kevin.macias.a@gmail.com>
2024-09-09 15:11:07 -0700

    feat: complete movie repository with full CRUD operations
    - Change from function behavior to class

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import rootRoute from './routes/rootRoute'
 import pagesRoute from './routes/pagesRoute'
 import mysqlPlugin from '@fastify/mysql'
 import { db, initDatabase } from './repositories/database'
-import { getMovies } from './repositories/movieRepository'
+import MovieRepository from './repositories/movieRepository'
 
 const server = fastify()
 
@@ -17,7 +17,8 @@ server.register(rootRoute, { prefix: "/v1" })
 
 server.after(() => {
   initDatabase(server)
-  getMovies().then(result => console.log(result))
+  const movies = new MovieRepository()
+  movies.getAll().then(value => console.log(value))
 })
 
 server.setErrorHandler(errorHandler)
--- src/repositories/movieRepository.ts
+++ src/repositories/movieRepository.ts
@@ -1,8 +1,58 @@
 import { db } from "./database";
 import Movie from "../entities/Movie";
 
-export async function getMovies() {
-  const query = `SELECT * FROM movies`
-  const [result] = await db.execute(query)
-  return result as Movie[]
+class MovieRepository {
+  async getAll(): Promise<Movie[]> {
+    const query = `SELECT * FROM movies`;
+    const [result] = await db.execute(query);
+    return result as Movie[];
+  }
+
+  async add(movie: Movie): Promise<any> {
+    const query = `INSERT INTO movies (id, title, description, image_url, genre, showtime,
+    duration, created_at, updated_at)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    const [result] = await db.execute(query, [
+      movie.id,
+      movie.title,
+      movie.description,
+      movie.imageUrl,
+      movie.genre,
+      movie.showtime,
+      movie.duration,
+      movie.createdAt,
+      movie.updatedAt,
+    ]);
+    return result;
+  }
+
+  async update(movie: Movie): Promise<any> {
+    const query = `UPDATE movies SET title = ?, description = ?, image_url = ?, genre = ?, 
+    showtime = ?, duration = ?, created_at = ?, updated_at = ? WHERE id = ?`;
+
+    const [result] = await db.execute(query, [
+      movie.title,
+      movie.description,
+      movie.imageUrl,
+      movie.genre,
+      movie.showtime,
+      movie.duration,
+      movie.createdAt,
+      movie.updatedAt,
+      movie.id,
+    ]);
+    return result;
+  }
+
+  async delete(param: string): Promise<any>;
+  async delete(param: Movie): Promise<any>;
+  async delete(param: string | Movie): Promise<any> {
+    const id = param instanceof Movie ? param.id : param;
+    const query = `DELETE FROM movies WHERE id = ?`;
+    const [result] = await db.execute(query, [id]);
+    return result;
+  }
 }
+
+export default MovieRepository;
