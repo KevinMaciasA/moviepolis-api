5cdfe6eb12099c11ea71326e7c7384b8b0219d25
Kevin <kevin.macias.a@gmail.com>
2024-09-10 20:38:22 -0700

    feat: add search by ID and title to movie repository; fix bugs

--- src/repositories/movieRepository.ts
+++ src/repositories/movieRepository.ts
@@ -2,15 +2,38 @@ import { db } from "./database";
 import Movie from "../entities/Movie";
 
 class MovieRepository {
+  private table: string = 'movies'
+
   async getAll(): Promise<Movie[]> {
-    const query = `SELECT * FROM movies`;
-    const [result] = await db.execute(query);
-    return result as Movie[];
+    const query = `SELECT * FROM ${this.table}`;
+    const [rows] = await db.execute(query);
+    return rows as Movie[];
+  }
+
+  async searchById(id: string): Promise<Movie | null> {
+    const query = `SELECT * FROM ${this.table} WHERE id = ?`
+    const [rows] = await db.execute(query, [id])
+    const movies = rows as Movie[];
+    return movies.length > 0 ? movies[0] : null;
+  }
+
+  async searchByTitle(title: string): Promise<Movie[] | null> {
+    const query = `SELECT * FROM ${this.table} WHERE title LIKE ?`
+    const searchTerm = `%${title}%`
+    const [rows] = await db.execute(query, [searchTerm]);
+    const movies = rows as Movie[];
+    return movies.length > 0 ? movies : null;
+  }
+
+  async exist(id: string): Promise<boolean> {
+    const movie = await this.searchById(id)
+
+    return !!movie
   }
 
   async add(movie: Movie): Promise<any> {
-    const query = `INSERT INTO movies (id, title, description, image_url, genre, showtime,
-    duration, created_at, updated_at)
+    const query = `INSERT INTO ${this.table} (id, title, description, image_url, genre,
+    showtime, duration, created_at, updated_at)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
     const [result] = await db.execute(query, [
@@ -28,8 +51,8 @@ class MovieRepository {
   }
 
   async update(movie: Movie): Promise<any> {
-    const query = `UPDATE movies SET title = ?, description = ?, image_url = ?, genre = ?, 
-    showtime = ?, duration = ?, created_at = ?, updated_at = ? WHERE id = ?`;
+    const query = `UPDATE ${this.table} SET title = ?, description = ?, image_url = ?,
+    genre = ?, showtime = ?, duration = ?, created_at = ?, updated_at = ? WHERE id = ?`;
 
     const [result] = await db.execute(query, [
       movie.title,
@@ -45,11 +68,11 @@ class MovieRepository {
     return result;
   }
 
-  async delete(param: string): Promise<any>;
-  async delete(param: Movie): Promise<any>;
+  async delete(id: string): Promise<any>;
+  async delete(movie: Movie): Promise<any>;
   async delete(param: string | Movie): Promise<any> {
     const id = param instanceof Movie ? param.id : param;
-    const query = `DELETE FROM movies WHERE id = ?`;
+    const query = `DELETE FROM ${this.table} WHERE id = ?`;
     const [result] = await db.execute(query, [id]);
     return result;
   }
